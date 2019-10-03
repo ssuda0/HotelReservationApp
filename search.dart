62a67bcb3e9f87339138160558145245acfecb58
LimSujin0 <21600590@handong.edu>
2019-10-03 21:31:31 +0900

    -1 -2 -3 finish step2

--- search.dart
+++ search.dart
@@ -6,16 +6,14 @@ class SearchScreen extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar : AppBar(
-          title : Text('Search'),
+        title : Text('Search'),
       ),
-      body : Criterias(),
+      body : ConditionSearch(),
     );
   }
 }
-
-
-class Criterias extends StatefulWidget {
-  CriteriaState createState() => new CriteriaState();
+class ConditionSearch extends StatefulWidget {
+  ConditonSearchState createState() => new ConditonSearchState();
 }
 
 class NewItem {
@@ -28,7 +26,7 @@ class NewItem {
 double discretevalue = 2.0;
 double hospitaldiscretevalue = 25.0;
 
-class CriteriaState extends State<Criterias> {
+class ConditonSearchState extends State<ConditionSearch> {
   List<NewItem> items = <NewItem>[
     new NewItem(
       false,
@@ -52,41 +50,57 @@ class CriteriaState extends State<Criterias> {
           child: new Column(
               children: <Widget>[
                 Text("select hotel Classes"),
-
+                MakeCheckBox(),
                 //put the children here
               ])
       ),
     ), //give all your items here
+
+    new NewItem(
+      false,
+      'Date',
+      new Padding(
+          padding: new EdgeInsets.all(20.0),
+      ),
+    ), //give all your items here
+
+    new NewItem(
+      false,
+      'Fee',
+      new Padding(
+        padding: new EdgeInsets.all(20.0),
+      ),
+    ),
   ];
 
   ListView List_Criteria;
   Widget build(BuildContext context) {
     List_Criteria = new ListView(
       children: [
-          new ExpansionPanelList(
-            expansionCallback: (int index, bool isExpanded) {
-              setState(() {
-                items[index].isExpanded = !items[index].isExpanded;
-              });
-            },
-            children: items.map((NewItem item) {
-              return new ExpansionPanel(
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return new ListTile(
-                      title: new Text(
-                        item.header,
-                        textAlign: TextAlign.left,
-                        style: new TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ));
-                },
-                isExpanded: item.isExpanded,
-                body: item.body,
-              );
-            }).toList(),
-          ),
+        new ExpansionPanelList(
+          expansionCallback: (int index, bool isExpanded) {
+            setState(() {
+              items[index].isExpanded = !items[index].isExpanded;
+            });
+          },
+          children: items.map((NewItem item) {
+            return new ExpansionPanel(
+              headerBuilder: (BuildContext context, bool isExpanded) {
+                return new ListTile(
+                    title: new Text(
+                      item.header,
+                      textAlign: TextAlign.left,
+                      style: new TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ));
+              },
+              isExpanded: item.isExpanded,
+              body: item.body,
+            );
+          }).toList(),
+        ),
       ],
     );
 
@@ -97,12 +111,99 @@ class CriteriaState extends State<Criterias> {
   }
 }
 
+class MakeCheckBox extends StatefulWidget{
+  @override
+  _MakeCheckBoxState createState() => _MakeCheckBoxState();
+}
+
+class _MakeCheckBoxState extends State<MakeCheckBox>{
+  bool _value1 = false;
+  bool _value2 = false;
+  bool _value3 = false;
+  bool _value4 = false;
+  bool _value5 = false;
+
+  void _value1Changed(bool value) => setState(() => _value1 = value);
+  void _value2Changed(bool value) => setState(() => _value2 = value);
+  void _value3Changed(bool value) => setState(() => _value3 = value);
+  void _value4Changed(bool value) => setState(() => _value4 = value);
+  void _value5Changed(bool value) => setState(() => _value5 = value);
+
+  Widget build(BuildContext context){
+    return new Column(
+      children: <Widget>[
+        new CheckboxListTile(
+          value: _value1,
+          onChanged: _value1Changed,
+          title: new Row(
+            children: <Widget>[
+              Icon(Icons.star, color:Colors.yellow, size:25.0),
+              Icon(Icons.star, color:Colors.yellow, size:25.0),
+              Icon(Icons.star, color:Colors.yellow, size:25.0 ),
+              Icon(Icons.star, color:Colors.yellow, size:25.0 ),
+              Icon(Icons.star, color:Colors.yellow, size:25.0 ),
+            ],
+          ),
+          controlAffinity: ListTileControlAffinity.leading,
+        ),
+        new CheckboxListTile(
+          value: _value2,
+          onChanged: _value2Changed,
+          title: new Row(
+            children: <Widget>[
+              Icon(Icons.star, color:Colors.yellow, size:25.0),
+              Icon(Icons.star, color:Colors.yellow, size:25.0),
+              Icon(Icons.star, color:Colors.yellow, size:25.0 ),
+              Icon(Icons.star, color:Colors.yellow, size:25.0 ),
+            ],
+          ),
+          controlAffinity: ListTileControlAffinity.leading,
+        ),
+        new CheckboxListTile(
+          value: _value3,
+          onChanged: _value3Changed,
+          title: new Row(
+            children: <Widget>[
+              Icon(Icons.star, color:Colors.yellow, size:25.0),
+              Icon(Icons.star, color:Colors.yellow, size:25.0),
+              Icon(Icons.star, color:Colors.yellow, size:25.0 ),
+            ],
+          ),
+          controlAffinity: ListTileControlAffinity.leading,
+        ),
+        new CheckboxListTile(
+          value: _value4,
+          onChanged: _value4Changed,
+          title: new Row(
+            children: <Widget>[
+              Icon(Icons.star, color:Colors.yellow, size:25.0),
+              Icon(Icons.star, color:Colors.yellow, size:25.0),
+            ],
+          ),
+          controlAffinity: ListTileControlAffinity.leading,
+        ),
+        new CheckboxListTile(
+          value: _value5,
+          onChanged: _value5Changed,
+          title: new Row(
+            children: <Widget>[
+              Icon(Icons.star, color:Colors.yellow, size:25.0),
+            ],
+          ),
+          controlAffinity: ListTileControlAffinity.leading,
+        ),
+
+      ],
+    );
+  }
+}
+
 class MakeRadioButton extends StatefulWidget {
   @override
-  _MyRadioButtonState createState() => _MyRadioButtonState();
+  _MakeRadioButtonState createState() => _MakeRadioButtonState();
 }
 enum SingingCharacter { Seoul, Busan, Daegu }
-class _MyRadioButtonState extends State<MakeRadioButton>{
+class _MakeRadioButtonState extends State<MakeRadioButton>{
   SingingCharacter _character = SingingCharacter.Seoul;
   Widget build(BuildContext context) {
     return Center(
@@ -149,4 +250,3 @@ class _MyRadioButtonState extends State<MakeRadioButton>{
     );
   }
 }
-
